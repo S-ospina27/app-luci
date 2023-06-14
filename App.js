33b51ad7be846af10db8ee35f4ab56e220b31a6a
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-06-14 16:30:23 -0500

    build

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+// import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useState } from "react";
+import Toast from "react-native-toast-message";
+import { AppNavigation } from "./src/navigation/AppNavigation";
+import AuthContext from "./src/validation/AuthContext";
 export default function App() {
+  const [token, setToken] = useState(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <AuthContext.Provider value={{ token, setToken }}>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </AuthContext.Provider>
+      <Toast visibilityTime={3000} />
+
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
